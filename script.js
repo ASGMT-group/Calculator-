42dfaf559654a5e89ed110a80d49aa4b39986a89
wondmD <wondmenehdereje@gamil.com>
2024-06-08 23:29:08 +0300

    evaluator function added

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function appendToDisplay(input) {
     display.value += input;
   }
 }
+function clearDisplay() {
+    display.value = "0";
+  }
+  
+  function calculate() {
+    try {
+      display.value = eval(display.value);
+    } catch (error) {
+      display.value = "Error";
+    }
+  }
+  
